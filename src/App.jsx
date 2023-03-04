f9b28af6e60e3ab0b7734b43beda9b333592afbf
eunseon Jeong <suni980215@gmail.com>
2023-03-04 10:58:30 +0900

    redux 함수를 이용한 클린코딩

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import counter from "./redux/modules/counter";
 import { type } from "@testing-library/user-event/dist/type";
+import { plusOne, minusOne } from "./redux/modules/counter";
 
 function App() {
   //여기에서 store에 접근하여, counter의 값을 읽어오고 싶다!
@@ -19,12 +20,10 @@ function App() {
   return (
     <>
       <div>현재카운트 : {number}</div>
-      
+
       <button
         onClick={() => {
-          dispatch({
-            type: "PLUS_ONE",
-          });
+          dispatch(plusOne());
         }}
       >
         플러스
@@ -32,9 +31,7 @@ function App() {
 
       <button
         onClick={() => {
-          dispatch({
-            type: "MINUS_ONE",
-          });
+          dispatch(minusOne());
         }}
       >
         마이너스
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,26 +1,42 @@
-//초기 상태 값(state)
+//여기서만 PLUS_ONE을 사용하는게 아니기때문에 export해줬다.
+export const PLUS_ONE = "PLUS_ONE";
+export const MINUS_ONE = "MINUS_ONE";
 
-const initialState = {
-    number: 0,
-  };
-
-//리듀서 : state에 변화를 일으키는 함수
-    //1.state를 action의 type에 따라 변경하는 함수
-
-//input값은 state와 action(store는 action을 type에 따라 state를 변경) 2가지를 받는다.
-const counter = (state = initialState ,action) =>{
-    switch(action.type){
-        case 'PLUS_ONE':
-            return {
-                number: state.number +1,
-            };
-        case 'MINUS_ONE':
-            return {
-                number: state.number -1,
-            }
-        default:
-            return state;
+//action creator : action value를 retrun하는 함수
+export const plusOne = () =>{
+    return {
+        type: PLUS_ONE,
     }
 }
+
+export const minusOne = () =>{
+    return {
+        type: MINUS_ONE,
+    }
+}
+
+//초기 상태 값(state)
+const initialState = {
+  number: 0,
+};
+
+//리듀서 : state에 변화를 일으키는 함수
+//1.state를 action의 type에 따라 변경하는 함수
+
+//input값은 state와 action(store는 action을 type에 따라 state를 변경) 2가지를 받는다.
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    case PLUS_ONE:
+      return {
+        number: state.number + 1,
+      };
+    case MINUS_ONE:
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
+};
 
 export default counter;
