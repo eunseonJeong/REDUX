f470f217c0dcad0f06ae0ac7248d7fbb13cbf37a
eunseon Jeong <suni980215@gmail.com>
2023-03-04 13:52:27 +0900

    redux (payload)

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-import counter from "./redux/modules/counter";
-import { type } from "@testing-library/user-event/dist/type";
-import { plusOne, minusOne } from "./redux/modules/counter";
+import { plusOne, minusOne, plusN , minusN } from "./redux/modules/counter";
+import { useState } from "react";
 
 function App() {
+  const [number,setNumber] = useState(0);
   //여기에서 store에 접근하여, counter의 값을 읽어오고 싶다!
   //useSelector(Redux의 hook) 매개변수로 함수가 들어간다.
 
-  const data = useSelector((state) => {
-    return state;
+  const counter = useSelector((state) => {
+    return state.counter;
   });
 
-  console.log(data);
   //리덕스의 hook
   const dispatch = useDispatch();
-  const number = useSelector((state) => state.counter.number);
+
+  const changeBtn = (e) =>{
+    setNumber(+e.target.value)
+  }
+
+  // //확인
+  // useEffect(() =>{
+  //   console.log('number->', number);
+  // },[number])
 
   return (
     <>
-      <div>현재카운트 : {number}</div>
+      <div>현재카운트 : {counter.number}</div>
+     <input type='number' value={number} onChange={changeBtn} />
 
       <button
         onClick={() => {
-          dispatch(plusOne());
+          // dispatch(plusOne());
+          dispatch(plusN(number))
         }}
       >
         플러스
@@ -31,7 +40,8 @@ function App() {
 
       <button
         onClick={() => {
-          dispatch(minusOne());
+          // dispatch(minusOne());
+          dispatch(minusN(number))
         }}
       >
         마이너스
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,17 +1,34 @@
 //여기서만 PLUS_ONE을 사용하는게 아니기때문에 export해줬다.
 export const PLUS_ONE = "PLUS_ONE";
 export const MINUS_ONE = "MINUS_ONE";
+export const PLUS_N = "PLUS_N";
+export const MINUS_N = "MINUS_N";
+
 
 //action creator : action value를 retrun하는 함수
-export const plusOne = () =>{
+export const plusOne = () => {
+  return {
+    type: PLUS_ONE,
+  };
+};
+
+export const minusOne = () => {
+  return {
+    type: MINUS_ONE,
+  };
+};
+
+export const plusN =(payload)=>{
     return {
-        type: PLUS_ONE,
+        type: PLUS_N,
+        payload,
     }
 }
 
-export const minusOne = () =>{
+export const minusN= (payload) => {
     return {
-        type: MINUS_ONE,
+        type: MINUS_N,
+        payload,
     }
 }
 
@@ -24,6 +41,9 @@ const initialState = {
 //1.state를 action의 type에 따라 변경하는 함수
 
 //input값은 state와 action(store는 action을 type에 따라 state를 변경) 2가지를 받는다.
+//action 객체라는 것은 action type을 payload 만큼 처리하는 것이다!
+//ex : payload가 3이다. 3만큼을 plus
+
 const counter = (state = initialState, action) => {
   switch (action.type) {
     case PLUS_ONE:
@@ -34,6 +54,14 @@ const counter = (state = initialState, action) => {
       return {
         number: state.number - 1,
       };
+      case PLUS_N:
+        return {
+            number: state.number + action.payload,
+        }
+        case MINUS_N:
+            return {
+                number: state.number - action.payload,
+            }
     default:
       return state;
   }
